ac6c8dcf0b10abd9080efb6e1c3d0b62864d2f99
LounaCharrieau <louna.charrieau@mmibordeaux.com>
2022-04-01 14:57:53 +0200

    style v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 
 
 Rails.application.routes.draw do
-  resources :reviews
   
-  root to:'reviews#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :reviews
+  root to:'reviews#index'
+
 end
